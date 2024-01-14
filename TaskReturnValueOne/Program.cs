17d4727add5a193d93b380dabc34d6d8e55eb774
zenin5479 <zenin5479@gmail.com>
2024-01-14 21:32:31 +0300

    Update 1.5 Обновлен метод

---
+++ TaskReturnValueOne/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TaskReturnValueOne
+{
+    class MyClass
+    {
+        long Operation()
+        {
+            Console.WriteLine("Идентификатор потока метода Operation: {0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            return Math.BigMul(1212, 2121);
+        }
+
+        public void OperationAsync()
+        {
+            Task<long> task = Task<long>.Factory.StartNew(Operation);
+            TaskAwaiter<long> awaiter = task.GetAwaiter();
+            Action continuation = () => Console.WriteLine("Результат: {0}", awaiter.GetResult());
+            awaiter.OnCompleted(continuation);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            MyClass my = new MyClass();
+            my.OperationAsync();
+            Console.WriteLine("Идентификатор потока метода Main: {0}", Thread.CurrentThread.ManagedThreadId);
+
+            // Задержка
+            Console.ReadKey();
+        }
+    }
+}
