7691a82eacafa331cb873357c9d663ed69f44c57
zenin5479 <zenin5479@gmail.com>
2024-01-14 20:12:32 +0300

    Update 0.6 Обновлен метод

--- AsyncAwaitTwo/Program.cs
+++ AsyncAwaitTwo/Program.cs
@@ -16,23 +16,21 @@ namespace AsyncAwaitTwo
 
         public async void OperationAsync()
         {
-            // Id потока совпадает с Id первичного потока. Это значит, что
-            // данный метод начинает выполняться в контексте первичного потока.
-            Console.WriteLine("OperationAsync (Part I) ThreadID {0}\n", Thread.CurrentThread.ManagedThreadId);
-
+            // Id потока совпадает с Id первичного потока.
+            // Это значит, что данный метод начинает выполняться в контексте первичного потока.
+            Console.WriteLine("Метод OperationAsync (Часть 1). Идентификатор потока {0}", Thread.CurrentThread.ManagedThreadId);
             Task task = new Task(Operation);
             task.Start();
             await task;
-
-            // Id потока совпадает с Id вторичного потока. Это значит, что
-            // данный метод заканчивает выполняться в контексте вторичного потока.
-            Console.WriteLine("\nOperationAsync (Part II) ThreadID {0}", Thread.CurrentThread.ManagedThreadId);
+            // Id потока совпадает с Id вторичного потока.
+            // Это значит, что данный метод заканчивает выполняться в контексте вторичного потока.
+            Console.WriteLine("Метод OperationAsync (Часть 2). Идентификатор потока {0}", Thread.CurrentThread.ManagedThreadId);
         }
 
 
         static void Main()
         {
-            Console.WriteLine("Идентификатор потока метода Main:  {0}\n", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Идентификатор потока метода Main: {0}", Thread.CurrentThread.ManagedThreadId);
             Program my = new Program();
             my.OperationAsync();
 
