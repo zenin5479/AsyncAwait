1196825918fff04b801b835b9f22a74f38a5bb47
zenin5479 <zenin5479@gmail.com>
2024-01-14 20:07:29 +0300

    Update 0.5 Обновлен метод

---
+++ AsyncAwaitTwo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AsyncAwaitTwo
+{
+    internal class Program
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Идентификатор потока метода Operation: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Метод Operation запущен");
+            Thread.Sleep(2000);
+            Console.WriteLine("Метод Operation завершен");
+        }
+
+        public async void OperationAsync()
+        {
+            // Id потока совпадает с Id первичного потока. Это значит, что
+            // данный метод начинает выполняться в контексте первичного потока.
+            Console.WriteLine("OperationAsync (Part I) ThreadID {0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            Task task = new Task(Operation);
+            task.Start();
+            await task;
+
+            // Id потока совпадает с Id вторичного потока. Это значит, что
+            // данный метод заканчивает выполняться в контексте вторичного потока.
+            Console.WriteLine("\nOperationAsync (Part II) ThreadID {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Идентификатор потока метода Main:  {0}\n", Thread.CurrentThread.ManagedThreadId);
+            Program my = new Program();
+            my.OperationAsync();
+
+            // Задержка
+            Console.ReadKey();
+        }
+    }
+}
