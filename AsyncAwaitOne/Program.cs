35d9e111ea4c2026fc6655942dbdeaa8a073caa2
zenin5479 <zenin5479@gmail.com>
2024-01-14 19:43:04 +0300

    Добавьте файлы проекта.

---
+++ AsyncAwaitOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitOne
+{
+    class MyClass
+    {
+        private void Operation()
+        {
+            Console.WriteLine("Идентификатор потока метода Operation: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Метод Operation запущен");
+            Thread.Sleep(2000);
+            Console.WriteLine("Метод Operation завершен");
+        }
+
+        public async void OperationAsync()
+        {
+            Task task = new Task(Operation);
+            task.Start();
+            await task;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Идентификатор потока метода Main: {0}", Thread.CurrentThread.ManagedThreadId);
+            MyClass my = new MyClass();
+            my.OperationAsync();
+
+            // Задержка
+            Console.ReadKey();
+        }
+    }
+}
