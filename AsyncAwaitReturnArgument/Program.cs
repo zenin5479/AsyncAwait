3bb6aa1166a00f35b794c0e7aa46e524455a5615
zenin5479 <zenin5479@gmail.com>
2024-01-15 11:48:41 +0300

    Update 2.1 Обновлен метод

--- AsyncAwaitReturnArgument/Program.cs
+++ AsyncAwaitReturnArgument/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace AsyncAwaitReturnArgument
 {
+    class MyClass
+    {
+        double Operation(object argument)
+        {
+            Thread.Sleep(2000);
+            return (double)argument * (double)argument;
+        }
+
+        public async Task<double> OperationAsync(double argument)
+        {
+            return await Task<double>.Factory.StartNew(Operation, argument);
+        }
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            MyClass my = new MyClass();
+            Task<double> task = my.OperationAsync(3);
+
+            task.ContinueWith(t => Console.WriteLine("Результат : {0}", t.Result));
+
+            // Delay
+            Console.ReadKey();
         }
     }
 }
