4b3c10980e844bfe2028d71853a622ce141f82de
zenin5479 <zenin5479@gmail.com>
2024-01-15 12:24:05 +0300

    Update 2.4 Обновлен метод

--- AsyncAwaitReturnArgumentDotPeek/Program.cs
+++ AsyncAwaitReturnArgumentDotPeek/Program.cs
@@ -1,61 +1,57 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwaitReturnArgumentDotPeek
 {
     class MyClass
     {
-        double Operation(object argument)
+        long Operation(object argument)
         {
-            Thread.Sleep(2000);
-            return (double)argument * (double)argument;
+            Console.WriteLine("Идентификатор потока метода Operation: {0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+            return Math.BigMul((int)argument, (int)argument);
         }
 
-        public Task<double> OperationAsync(double argument)
+        public Task<long> OperationAsync(int argument)
         {
             AsyncStateMachine stateMachine;
-            stateMachine.outer = this;
-            stateMachine.builder = AsyncTaskMethodBuilder<double>.Create();
-            stateMachine.state = -1;
-            stateMachine.argument = argument;
-
-            stateMachine.builder.Start(ref stateMachine);
-
-            return stateMachine.builder.Task;
+            stateMachine.Outer = this;
+            stateMachine.Builder = AsyncTaskMethodBuilder<long>.Create();
+            stateMachine.State = -1;
+            stateMachine.Argument = argument;
+            stateMachine.Builder.Start(ref stateMachine);
+            return stateMachine.Builder.Task;
         }
 
         struct AsyncStateMachine : IAsyncStateMachine
         {
-            public AsyncTaskMethodBuilder<double> builder;
-            public MyClass outer;
-            public int state;
-            public double argument;
-
-            TaskAwaiter<double> awaiter;
+            public AsyncTaskMethodBuilder<long> Builder;
+            public MyClass Outer;
+            public int State;
+            public int Argument;
+            TaskAwaiter<long> _awaiter;
 
             void IAsyncStateMachine.MoveNext()
             {
-                if (state == -1)
+                if (State == -1)
                 {
-                    Func<object, double> function = outer.Operation;
-                    Task<double> task = Task<double>.Factory.StartNew(function, argument);
-                    awaiter = task.GetAwaiter();
-
-                    state = 0;
-
-                    builder.AwaitOnCompleted(ref awaiter, ref this);
+                    Func<object, long> function = Outer.Operation;
+                    Task<long> task = Task<long>.Factory.StartNew(function, Argument);
+                    _awaiter = task.GetAwaiter();
+                    State = 0;
+                    Builder.AwaitOnCompleted(ref _awaiter, ref this);
                     return;
                 }
 
-                double result = awaiter.GetResult();
-                builder.SetResult(result);
+                long result = _awaiter.GetResult();
+                Builder.SetResult(result);
             }
 
             void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
             {
-                builder.SetStateMachine(stateMachine);
+                Builder.SetStateMachine(stateMachine);
             }
         }
     }
@@ -65,7 +61,7 @@ namespace AsyncAwaitReturnArgumentDotPeek
         static void Main()
         {
             MyClass my = new MyClass();
-            Task<double> task = my.OperationAsync(3);
+            Task<long> task = my.OperationAsync(2121212121);
             Console.WriteLine("Первичный поток завершил работу. Идентификатор потока метода {0}", Thread.CurrentThread.ManagedThreadId);
             task.ContinueWith(t => Console.WriteLine("Результат : {0}", t.Result));
 
